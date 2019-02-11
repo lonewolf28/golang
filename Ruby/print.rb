3b1a927ae42b4b948283de4902bb1f87bb79d22d
Raj Barath <raajbarath86@gmail.com>
2019-02-11 11:09:37 -0500

    Deleted

--- Ruby/print.rb
+++
@@ -1,7 +0,0 @@
-#!/home/raj/.rvm/rubies/ruby-2.4.1/bin/ruby
-
-pdf = Dir["/home/raj/srv/www/shop.medreleaf.com/current/web/app/uploads/consent-forms/*"]
-
-pdf.each do |p|
-	 system("/usr/bin/lpr #{p} ")
-end
